9be0f8121589249c36488ed52e186c01ecc45a16
Shalin Shah <shalinvs@gmail.com>
2014-04-21 12:31:42 -0700

    Revert f271ff8..2b2d4fc
    This rolls back to commit f271ff865cab589c40a1f837f23dd84263b522d4.

---
+++ Speak/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Speak
+//
+//  Created by Shalin Shah on 4/18/14.
+//  Copyright (c) 2014 Shalin Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "storyboardAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([storyboardAppDelegate class]));
+    }
+}
---
+++ Speak/storyboardViewController.h
@@ -0,0 +1,15 @@
+//
+//  storyboardViewController.h
+//  Speak
+//
+//  Created by Shalin Shah on 4/18/14.
+//  Copyright (c) 2014 Shalin Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <TesseractOCR/TesseractOCR.h>
+
+@interface storyboardViewController : UIViewController <TesseractDelegate>
+
+
+@end
