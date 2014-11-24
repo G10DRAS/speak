120e14ae5f148f2b20c6b2760174ad12b4f3967b
Shalin Shah <shalinvs@gmail.com>
2014-11-24 15:10:53 -0800

    Added a SettingsViewController

---
+++ Voice/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Voice
+//
+//  Created by Shalin Shah on 11/24/14.
+//  Copyright (c) 2014 Shalin Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
